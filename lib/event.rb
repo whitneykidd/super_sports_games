b891d129bd276cc804cbb5584c6b5c40789f593a
Whitney Kidd <whitneymkidd@gmail.com>
2020-01-30 15:37:47 -0700

    Add average_age

--- lib/event.rb
+++ lib/event.rb
@@ -7,18 +7,19 @@ class Event
   end
 
   def max_age (max_age)
-    age.max
+    @age.max
   end
 
   def min_age (min_age)
-    age.min
+    @age.min
   end
-
-
-
-
-
-
-
+  
+  def average_age
+   sum = 0
+   @ages.each do |age|
+     sum += age
+   end
+   sum.round(1) / @ages.count
+ end
 
 end
