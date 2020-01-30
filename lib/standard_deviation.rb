4378421b94fca9d982349d14a14da19e3f1abb21
Whitney Kidd <whitneymkidd@gmail.com>
2020-01-30 13:26:40 -0700

    Add def ages_sum

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,5 +1,14 @@
 ages = [24, 30, 18, 20, 41]
 
 # Your code here for calculating the standard deviation
+def ages_sum(array)
+  sum = 0
+  array.each do |age|
+    sum += age
+  end
+  sum
+end
+
+
 
 # When you find the standard deviation, print it out
