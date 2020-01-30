224cf0b382a40d71ea7fc8d9dfa69b872ae59cfb
Whitney Kidd <whitneymkidd@gmail.com>
2020-01-30 14:05:04 -0700

    Add ages_subtract

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -1,22 +1,36 @@
-ages = [24, 30, 18, 20, 41]
+# ages = [24, 30, 18, 20, 41]
+class Ages
+  attr_reader :ages
 
-# Your code here for calculating the standard deviation
-def ages_sum(array)
-  sum = 0
-  array.each do |age|
-    sum += age
+  def initialize(ages)
+    @ages = ages
   end
-  sum
-end
 
-def ages_index(array)
-  array.count
-end
+  def ages_sum(array)
+    sum = 0
+    array.each do |age|
+      sum += age
+    end
+    sum
+  end
 
-def ages_average(array)
-  ages.ages_sum / ages.ages_index
-end
+  def ages_index(array)
+    array.count
+  end
 
+  def ages_average(sum, index)
+    sum / index
+  end
+
+  def ages_subtract(array, average)
+    subtracted = []
+    array.each do |age|
+      subtracted << age - average
+    end
+    subtracted
+  end
+
+end
 
 
 
