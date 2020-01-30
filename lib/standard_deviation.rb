3ba930b3cd47a96ded00fafde1dee82591df4cde
Whitney Kidd <whitneymkidd@gmail.com>
2020-01-30 13:39:34 -0700

    Add ages_index and ages_average

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -9,6 +9,20 @@ def ages_sum(array)
   sum
 end
 
+def ages_index(array)
+  array.count
+end
 
+def ages_average(array)
+  ages.ages_sum / ages.ages_index
+end
+
+
+
+
+
+# ages = [24, 30, 18, 20, 41]
+# puts ages_sum
+# puts ages_index
 
 # When you find the standard deviation, print it out
